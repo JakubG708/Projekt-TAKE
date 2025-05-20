1aeeeb614ad5e67405f0612f1a4d5b9e1e277e5d
JakubG708 <agnel113322@gmail.com>
2025-05-20 13:50:33 +0200

    naprawiona struktura paczek projektu

--- src/main/java/com/example/demo/isItWorking.java
+++ src/main/java/com/example/demo/isItWorking.java
@@ -1,16 +1,23 @@
 package com.example.demo;
 
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.models.*;
 
 @RestController
 @CrossOrigin
 public class isItWorking {
 
 	@GetMapping("/test")
-	public String test()
-	{
-		return "Test string";
-	}
+	 public Car test() {
+        Car car = new Car();
+        car.setCarId(1);
+        car.setBrand("Toyota");
+
+        return car;  // Zostanie automatycznie zamieniony na JSON
+    }
 }
--- src/main/java/com/example/demo/models/Car.java
+++ src/main/java/com/example/demo/models/Car.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.demo.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,7 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -20,4 +25,7 @@ public class Car {
     @Column(nullable = false)
     private String brand;
 
+
+    
+    
 }
